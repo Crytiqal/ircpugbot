1d6b81a697dd9a425767e6f1f6bd68d690b9b317
Crytiqal <crytiqal@team-aero.org>
2015-03-31 01:57:51 +0200

    update

---
+++ ircbots/battlebot/battlebot_test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ echo IRCBot
+ echo ----------
+ echo [irc]: IRC server
+ echo [channel]: IRC channel
+ echo ----------
+ php -q "/home/ubuntu/workspace/ircbots/battlebot/battlebot.php"
+ echo ----------
+ pause
