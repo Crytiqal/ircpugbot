c9e4ef7f2260ca38cfaed946c5a676e34edec0d4
Crytiqal <Crytiqal@live.com>
2015-04-11 15:31:05 -0400

    cleaned

--- ircbots/battlebot/battlebot_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
- echo IRCBot
- echo ----------
- echo [irc]: IRC server
- echo [channel]: IRC channel
- echo ----------
- php -q "/path/to/ircservers/ircbots/battlebot/battlebot.php"
- echo ----------
- pause
