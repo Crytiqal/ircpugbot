ef9e118838475c06f2188db90011a86098795436
Crytiqal.Aero <Crytiqal@live.com>
2020-12-23 19:24:49 +0100

    Delete bb.sh

--- ircbots/bb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
- echo IRCBot
- echo ----------
- echo [irc]: IRC server
- echo [channel]: IRC channel
- echo ----------
- php -q "/srv/irc/bots/battlebot/battlebot.php"
- echo ----------
- pause
